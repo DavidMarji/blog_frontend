d6204a928e47abf591a8aacd67cf0a2912c3c854
DavidMarji <david.marji@mail.mcgill.ca>
2024-07-18 14:57:40 +0300

    fixed exports in routerFunctions.js

--- blog_frontend/src/utilities/routerFunctions.js
+++ blog_frontend/src/utilities/routerFunctions.js
@@ -1,23 +1,21 @@
 import { useRouter } from 'vue-router';
 const router = useRouter();
 
-export default {
-    navigateToHome : function navigateToHome() {
-        router.push('/home');
-    },
-    navigateToLogin : function navigateToLogin() {
-        router.push('/login');
-    },
-    navigateToSignup : function navigateToSignup() {
-        router.push('/signUp');
-    },
-    navigateToResult : function navigateToResult(title) {
-        router.push(`/result/:${title}`);
-    },
-    navigateToUserProfile : function navigateToUserProfile(username) {
-        router.push(`/users/${username}`);
-    },
-    navigateToBlog : function navigateToBlog(blogId, pageNumber) {
-        router.push(`/blogs/${blogId}/${pageNumber}`);
-    }
-  }
+export const navigateToHome = function navigateToHome() {
+    router.push('/home');
+}
+export const navigateToLogin = function navigateToLogin() {
+    router.push('/login');
+}
+export const navigateToSignup = function navigateToSignup() {
+    router.push('/signUp');
+}
+export const navigateToResult = function navigateToResult(title) {
+    router.push(`/result/:${title}`);
+}
+export const navigateToUserProfile = function navigateToUserProfile(username) {
+    router.push(`/users/${username}`);
+}
+export const navigateToBlog = function navigateToBlog(blogId, pageNumber) {
+    router.push(`/blogs/${blogId}/${pageNumber}`);
+}
